b7b46f706be33fa8ae23dbb6a59de26d6880f1c7
arcanjo96 <lucasarcanjo.ld@gmail.com>
2019-08-26 17:22:12 -0300

    first commit

---
+++ database/migrations/1566847113148_user_image_fk_schema.js
@@ -0,0 +1,22 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+
+class UserImageFkSchema extends Schema {
+  up() {
+    this.table('users', (table) => {
+      table.foreign('image_id').references('id').inTable('images').onDelete('cascade')
+      // alter table
+    })
+  }
+
+  down() {
+    this.table('users', (table) => {
+      // reverse alternations
+      table.dropForeign('image_id')
+    })
+  }
+}
+
+module.exports = UserImageFkSchema
